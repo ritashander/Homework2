87aaa3c81caa91e672620adeaefdcaeda20071c1
Rita Shander <iamshizooo@gmail.com>
2023-01-26 14:23:27 +0300

    first work done

---
+++ Homework2/work1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 100;
+System.Console.WriteLine(result / 10);
